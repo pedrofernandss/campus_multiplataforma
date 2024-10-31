10bcd8514fde55977c514d7f346a3edad4075f3b
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-10-31 20:22:37 -0300

    :sparkles: feat: add firebase database

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
