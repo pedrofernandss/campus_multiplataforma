51828bc51f02d254c42f5d35c3da2ec9246f37f4
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-12-23 22:30:44 -0300

    💄 style: add icon png files

--- constants/icons.js
+++ constants/icons.js
@@ -1,5 +1,7 @@
 import menuIcon from "../assets/icons/menu-icon.png";
 import trendingIcon from "../assets/icons/trending-up-icon.png";
-import clockIcon from "../assets/icons/clock-icon.png"
+import clockIcon from "../assets/icons/clock-icon.png";
+import loginIcon from "../assets/icons/login-icon.png";
+import bugIcon from "../assets/icons/bug-icon.png"
 
-export default { menuIcon, trendingIcon, clockIcon };
+export default { menuIcon, trendingIcon, clockIcon, loginIcon, bugIcon };
