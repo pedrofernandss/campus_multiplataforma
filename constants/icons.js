16d797ee82855afeab0fc49913807ed00fc63531
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-12-23 23:07:27 -0300

    💄 style: add arrow foward icon

--- constants/icons.js
+++ constants/icons.js
@@ -2,6 +2,7 @@ import menuIcon from "../assets/icons/menu-icon.png";
 import trendingIcon from "../assets/icons/trending-up-icon.png";
 import clockIcon from "../assets/icons/clock-icon.png";
 import loginIcon from "../assets/icons/login-icon.png";
-import bugIcon from "../assets/icons/bug-icon.png"
+import bugIcon from "../assets/icons/bug-icon.png";
+import arrowFowardIcon from "../assets/icons/arrow-foward-icon.png"
 
-export default { menuIcon, trendingIcon, clockIcon, loginIcon, bugIcon };
+export default { menuIcon, trendingIcon, clockIcon, loginIcon, bugIcon, arrowFowardIcon };
