c99ccbe4e1d65cc5385a13da425b266f9c8f6c0a
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-12-29 19:12:09 -0300

    ✨ feat: fetch news from firebase

---
+++ functions/newsFunctions.ts
@@ -0,0 +1,17 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { News } from "../constants/types";
+
+export const fetchNews = async (): Promise<News[]> => {
+    try {
+        const response = await getDocs(collection(db, "newsTest")); 
+        const news = response.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data(),
+        })) as News[];
+        return news;
+    } catch (error) {
+        console.error("Erro ao buscar as noticias: ", error);
+        throw error;
+    }
+};
