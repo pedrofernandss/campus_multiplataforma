e33a57d36ab54f09643832146a11cc9fbaa13b1f
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2024-12-29 19:13:33 -0300

    ✨ feat: fetch news from firebase

--- components/NewsList.tsx
+++ components/NewsList.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dimensions, FlatList, SafeAreaView, StyleSheet } from 'react-native';
 import newsData from '@/assets/mocked-data/news';
 import NewsCard from './NewsCard';
+import { types } from '@/constants';
+import { fetchNews } from '@/functions/newsFunctions';
 
 const { width } = Dimensions.get('window');
 
 const NewsList = () => {
+  const [news, setNews] = useState<types.News[]>([]);
+  
+  useEffect(() => {
+      const loadNews = async () => {
+          try {
+              const fetchedNews = await fetchNews();
+              setNews(fetchedNews);
+          } catch (error) {
+              console.error("Erro ao buscar as tags: ", error);
+          }
+      };
+
+      loadNews();
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FlatList
-        data={newsData}
-        renderItem={({ item }) => <NewsCard item={item} />}
-        keyExtractor={(item) => item.id}
+        data={news}
+        keyExtractor={(news) => news.id.toString()}
+        renderItem={({ item }) => <NewsCard news={item} />}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.flatListContainer}
       />
@@ -23,6 +40,6 @@ export default NewsList;
 
 const styles = StyleSheet.create({
   flatListContainer: {
-    paddingTop: 10, 
+    paddingTop: 15, 
   },
 });
