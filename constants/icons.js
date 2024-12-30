42c9dd723c0739ba573e83fdfcd2ccd643a4e4e6
IthaloDekki <ithalomedei76@gmail.com>
2024-12-30 18:59:48 -0300

    💄 uptading the icons and update the imports

--- constants/icons.js
+++ constants/icons.js
@@ -6,6 +6,7 @@ import bugIcon from "../assets/icons/bug-icon.png";
 import arrowFowardIcon from "../assets/icons/arrow-foward-icon.png"
 import calendarIcon from "../assets/icons/calendar-icon.png";
 import descriptionIcon from "../assets/icons/description-icon.png";
-import homeIcon from "../assets/icons/home-icon.png"
+import homeIcon from "../assets/icons/home-icon.png";
+import logoutIcon from "../assets/icons/logout-icon.png";
 
-export default { menuIcon, trendingIcon, clockIcon, loginIcon, bugIcon, arrowFowardIcon, calendarIcon, descriptionIcon, homeIcon };
+export default { menuIcon, trendingIcon, clockIcon, loginIcon, bugIcon, arrowFowardIcon, calendarIcon, descriptionIcon, homeIcon, logoutIcon };
