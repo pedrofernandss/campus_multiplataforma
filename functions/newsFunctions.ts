ca85c7dae303c228a3a953c697d69dbce4fed66a
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-01-07 17:43:24 -0300

    ✨ feat: fetch only news with 'published' attribute set to true

--- functions/newsFunctions.ts
+++ functions/newsFunctions.ts
@@ -1,10 +1,10 @@
-import { collection, getDocs, query, orderBy  } from "firebase/firestore";
+import { collection, getDocs, query, orderBy, where  } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { News } from "../constants/types";
 
 export const fetchNews = async (): Promise<News[]> => {
     try {
-        const response = await getDocs(query(collection(db, "news"), orderBy("createdAt", "desc"))); 
+        const response = await getDocs(query(collection(db, "news"), where("published", "==", true))); 
         const news = response.docs.map(doc => ({
             id: doc.id,
             ...doc.data(),
