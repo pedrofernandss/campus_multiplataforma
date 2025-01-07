3ca9405c581608390c55c341ec7238780827e44a
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-01-06 21:51:01 -0300

    🍱 add right click icon to assets

--- constants/icons.js
+++ constants/icons.js
@@ -8,5 +8,6 @@ import calendarIcon from "../assets/icons/calendar-icon.png";
 import descriptionIcon from "../assets/icons/description-icon.png";
 import homeIcon from "../assets/icons/home-icon.png";
 import logoutIcon from "../assets/icons/logout-icon.png";
+import rightClick from "../assets/icons/right-click-icon.png"
 
-export default { menuIcon, trendingIcon, clockIcon, loginIcon, bugIcon, arrowFowardIcon, calendarIcon, descriptionIcon, homeIcon, logoutIcon };
+export default { menuIcon, trendingIcon, clockIcon, loginIcon, bugIcon, arrowFowardIcon, calendarIcon, descriptionIcon, homeIcon, logoutIcon, rightClick };
