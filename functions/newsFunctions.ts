23d9726fe5a043e65b127a97113ef46d06f62d7b
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-01-07 22:17:57 -0300

    ✨ feat: add function to calculate relative time since news creation

--- functions/newsFunctions.ts
+++ functions/newsFunctions.ts
@@ -18,3 +18,27 @@ export const fetchNews = async (): Promise<News[]> => {
         throw error;
     }
 };
+
+export const getRelativeTime = (dateString: string): string => {
+    const now = new Date();
+    const createdAt = new Date(dateString);
+    const diffInMilliseconds = now.getTime() - createdAt.getTime();
+    const units: {[key:string]: number} = {
+        'ano': 1000 * 3600 * 24 * 365,  
+        'mês': 1000 * 3600 * 24 * 30,   
+        'sem': 1000 * 3600 * 24 * 7,    
+        'dia': 1000 * 3600 * 24,        
+        'hora': 1000 * 3600,             
+        'min': 1000 * 60 
+    };
+
+    for(const [unit, millisecondsInUnit] of Object.entries(units)){
+        const diff = Math.floor(diffInMilliseconds / millisecondsInUnit);
+        if(diff >= 1){
+            return `${diff} ${unit}`
+        }
+    }
+
+    return 'Agora'
+
+}
