087d6dab8eab178e1d1edd1591dc841f8fedf074
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-01-17 15:23:38 -0300

    💄 feat: add sugestion news red icon

--- constants/icons.js
+++ constants/icons.js
@@ -10,6 +10,7 @@ import homeIcon from "../assets/icons/home-icon.png";
 import logoutIcon from "../assets/icons/logout-icon.png";
 import rightClick from "../assets/icons/right-click-icon.png"
 import sugestNews from "../assets/icons/sugest-news-icon.png"
+import redSugestNews from "../assets/icons/red-sugest-news-icon.png"
 
 export default { menuIcon, trendingIcon, clockIcon, loginIcon, bugIcon, arrowFowardIcon, 
-    calendarIcon, descriptionIcon, homeIcon, logoutIcon, rightClick, sugestNews };
+    calendarIcon, descriptionIcon, homeIcon, logoutIcon, rightClick, sugestNews, redSugestNews };
