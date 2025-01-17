fc9b0fe16088801c20a1ed334c6dd1655735e85b
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-01-17 15:24:41 -0300

    ✨ feat: create sendSugestionNewsEmail function

---
+++ functions/emailFunctions.ts
@@ -0,0 +1,22 @@
+import emailjs from 'emailjs-com';
+
+emailjs.init(process.env.EXPO_PUBLIC_USER_ID_EMAILJS || '');
+
+export const sendSugestionNewsEmail = async (template: string, sugestion: string) => {
+  console.log(sugestion)
+  const serviceID = process.env.EXPO_PUBLIC_EMAIL_SERVICE_ID || ''; 
+  const templateID = template; 
+
+  var templateParams = {
+    to_name: 'Campusito',
+    message: sugestion,  
+  }
+
+  emailjs.send(serviceID, templateID, templateParams).then(
+    (response) => {
+      console.log('E-mail enviado com sucesso!', response.status, response.text);
+  })
+  .catch((error) => {
+    console.error('Erro ao enviar e-mail:', error);
+  });
+};
