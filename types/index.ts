0c5ad8356845c242894f87fe14725b5d953513d5
Pedro Henrique <pedrohenriquemonteironasciment@gmail.com>
2025-01-17 17:55:45 -0300

    🐛 fix: fixed types for news

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+import * as type1 from "./drawerButton";
+import * as type2 from "./instagramReels";
+import * as type3 from "./news";
+import * as type4 from "./tag";
+
+export default {
+  type1,
+  type2,
+  type3,
+  type4
+};
