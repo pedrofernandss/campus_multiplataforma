70fbf4630d966945280ac2257761a63c51719532
IthaloDekki <ithalomedei76@gmail.com>
2025-01-30 17:28:24 -0300

    🎨 style: deixando código mais legível

--- constants/icons.js
+++ constants/icons.js
@@ -8,6 +8,29 @@ import calendarIcon from "../assets/icons/calendar-icon.png";
 import descriptionIcon from "../assets/icons/description-icon.png";
 import homeIcon from "../assets/icons/home-icon.png";
 import logoutIcon from "../assets/icons/logout-icon.png";
-import rightClick from "../assets/icons/right-click-icon.png"
+import rightClick from "../assets/icons/right-click-icon.png";
+import trashIcon from "../assets/icons/delete-icon.png";
+import checkIcon from "../assets/icons/check-icon.png";
+import editIcon from "../assets/icons/edit-icon.png";
+import taskIcon from "../assets/icons/task-icon.png";
+import timerIcon from "../assets/icons/timer-icon.png";
 
-export default { menuIcon, trendingIcon, clockIcon, loginIcon, bugIcon, arrowFowardIcon, calendarIcon, descriptionIcon, homeIcon, logoutIcon, rightClick };
+
+export default { 
+    menuIcon, 
+    trendingIcon, 
+    clockIcon, 
+    loginIcon, 
+    bugIcon, 
+    arrowFowardIcon, 
+    calendarIcon, 
+    descriptionIcon, 
+    homeIcon, 
+    logoutIcon,
+    rightClick, 
+    trashIcon, 
+    checkIcon, 
+    editIcon, 
+    taskIcon, 
+    timerIcon 
+};
