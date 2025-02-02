0e4dfe4163667b649dc1f8bb634f0678bae76331
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-02-01 23:48:16 -0300

    🐛 fix: set as optional inputValue

--- types/modal.ts
+++ types/modal.ts
@@ -3,13 +3,13 @@ import { icons } from "@/constants";
 export interface ModalBox {
     title?: string,
     label: string
-    icon: keyof typeof icons;
+    icon?: keyof typeof icons;
     isOpen: boolean
     hasInput?: boolean
     onConfirmButton?: () => void;
     onCancelButton?: () => void;
     confirmButtonText?: string;
     cancelButtonText?: string;
-    inputValue: string; // Valor do input
-    onInputChange: (text: string) => void;   
+    inputValue?: string; // Valor do input
+    onInputChange?: (text: string) => void;   
 }
