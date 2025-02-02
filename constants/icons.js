b739a2f7ad155d96f0b0a109e6c891b2af9be8ee
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-02-01 23:47:00 -0300

    🔀 correcting merge problems

--- constants/icons.js
+++ constants/icons.js
@@ -8,39 +8,18 @@ import calendarIcon from "../assets/icons/calendar-icon.png";
 import descriptionIcon from "../assets/icons/description-icon.png";
 import homeIcon from "../assets/icons/home-icon.png";
 import logoutIcon from "../assets/icons/logout-icon.png";
-<<<<<<< HEAD
 import rightClick from "../assets/icons/right-click-icon.png";
 import trashIcon from "../assets/icons/delete-icon.png";
 import checkIcon from "../assets/icons/check-icon.png";
 import editIcon from "../assets/icons/edit-icon.png";
 import taskIcon from "../assets/icons/task-icon.png";
 import timerIcon from "../assets/icons/timer-icon.png";
-
-
-export default { 
-    menuIcon, 
-    trendingIcon, 
-    clockIcon, 
-    loginIcon, 
-    bugIcon, 
-    arrowFowardIcon, 
-    calendarIcon, 
-    descriptionIcon, 
-    homeIcon, 
-    logoutIcon,
-    rightClick, 
-    trashIcon, 
-    checkIcon, 
-    editIcon, 
-    taskIcon, 
-    timerIcon 
-};
-=======
-import rightClick from "../assets/icons/right-click-icon.png"
 import sugestNews from "../assets/icons/sugest-news-icon.png"
 import redSugestNews from "../assets/icons/red-sugest-news-icon.png"
 import redBugIcon from "../assets/icons/red-bug-icon.png";
+import reportIcon from "../assets/icons/report-red-icon.png"
+
 
 export default { menuIcon, trendingIcon, clockIcon, loginIcon, bugIcon, arrowFowardIcon, 
-    calendarIcon, descriptionIcon, homeIcon, logoutIcon, rightClick, sugestNews, redSugestNews, redBugIcon };
->>>>>>> feature/modals
+    calendarIcon, descriptionIcon, homeIcon, logoutIcon, rightClick, trashIcon, 
+    checkIcon, editIcon, taskIcon, timerIcon, sugestNews, redSugestNews, redBugIcon, reportIcon };
--- constants/index.js
+++ constants/index.js
@@ -1,8 +1,3 @@
 import icons from "./icons";
 import images from "./images";
-
-<<<<<<< HEAD
 export { icons, images};
-=======
-export { icons, images };
->>>>>>> feature/modals
