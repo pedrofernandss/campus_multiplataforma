6b4dbe24e00b8792fcf0cbe6b6df7c563b6521f8
Pedro Nascimento <pedrohenriquemonteironasciment@gmail.com>
2025-02-25 19:20:40 -0300

    🐛 fix: tags section animation fix

--- components/Trendings.tsx
+++ components/Trendings.tsx
@@ -11,6 +11,7 @@ const Trendings: React.FC = () => {
 
     const [tags, setTags] = useState<types.Tag[]>([]);
     const translateX = useRef(new Animated.Value(0)).current;
+    const [isScrolling, setIsScrolling] = useState(false);
 
     useEffect(() => {
         const loadTags = async () => {
@@ -28,11 +29,10 @@ const Trendings: React.FC = () => {
     useEffect(() => {
         const totalWidth = tags.length * 150;
         const startAnimation = () => {
-            if (totalWidth > 0) {
-                translateX.setValue(0);
+            if (totalWidth > 0 && !isScrolling) {
                 Animated.timing(translateX, {
-                    toValue: -totalWidth+500,
-                    duration: 13000,
+                    toValue: -totalWidth,
+                    duration: 20000,
                     useNativeDriver: true,
                 }).start(() => {
                     translateX.setValue(0);
@@ -46,7 +46,7 @@ const Trendings: React.FC = () => {
         return () => {
             translateX.stopAnimation();
         };
-    }, [translateX, tags]);
+    }, [translateX, tags, isScrolling]);
 
     return (
         <View>
@@ -58,14 +58,27 @@ const Trendings: React.FC = () => {
                 />
                 <Text style={styles.labelText}>Em alta</Text>
                 <View style={styles.overflowTransformation}>
-                    <Animated.View style={[{ transform: [{ translateX }], width: tags.length*150}, styles.animatedTrendingText]}>
+                    <Animated.View style={[
+                        styles.animatedTrendingText,
+                        { 
+                            transform: [{ translateX }],
+                            width: tags.length * 150 * 2,
+                        }
+                    ]}>
                         <FlatList
-                            data={tags}
+                            data={[...tags, ...tags]}
                             horizontal
-                            keyExtractor={(tag) => tag.id.toString()}
-                            renderItem={({ item }) => <TrendingItem tag={item}/> }
-                            scrollEnabled={false}
+                            keyExtractor={(tag, index) => `${tag.id}-${index}`}
+                            renderItem={({ item }) => <TrendingItem tag={item}/>}
                             showsHorizontalScrollIndicator={false}
+                            onScrollBeginDrag={() => {
+                                setIsScrolling(true);
+                                translateX.stopAnimation();
+                            }}
+                            onScrollEndDrag={() => {
+                                setIsScrolling(false);
+                            }}
+                            scrollEnabled={true}
                         />
                     </Animated.View>
                 </View>
